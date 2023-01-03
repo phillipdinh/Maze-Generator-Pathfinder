740c050bbca0a4184b70f1c92a74cd58d06ed811
phillipdinh <phillipdinh11@yahoo.com>
2023-01-02 16:54:37 -0800

    Buttons will not reset maze before starting

--- src/components/Maze.js
+++ src/components/Maze.js
@@ -1,16 +1,21 @@
 import React from "react";
 import Node from "./Node";
 
+// Used to delay maze graphics
 function delay(ms) {
   return new Promise((resolve) => {
     setTimeout(resolve, ms);
   });
 }
 
+// Used by:
+// RBT_DFS(), solveRBTDFS(), prim()
 function getRand(arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 }
 
+// Used by:
+// RBT_DFS(), prim()
 const oppDir = {
   top: "bottom",
   bottom: "top",
@@ -18,6 +23,7 @@ const oppDir = {
   right: "left",
 };
 
+// Used by mazeInit and returns node object with props
 const createNode = (row, col) => {
   return {
     col,
@@ -31,11 +37,15 @@ const createNode = (row, col) => {
   };
 };
 
+// Initializes 16 x 16 maze of nodes
+// Each node has correct column and row,
+// All 4 walls and visited are false,
+// Id is not-active
 const mazeInit = () => {
   const grid = [];
-  for (let x = 0; x < 15; x++) {
+  for (let x = 0; x < 16; x++) {
     const currRow = [];
-    for (let y = 0; y < 15; y++) {
+    for (let y = 0; y < 16; y++) {
       currRow.push(createNode(y, x));
     }
     grid.push(currRow);
@@ -45,19 +55,35 @@ const mazeInit = () => {
 
 //////////////////// Maze Component ///////////////////////////////
 export default function Maze() {
+  // Initializes and set maze to new maze
   function clearMaze() {
-    setMaze(mazeInit());
+    maze.map((row, rowIdx) => {
+      row.map((node, colIdx) => {
+        updateMaze(colIdx, rowIdx, "left", false);
+        updateMaze(colIdx, rowIdx, "right", false);
+        updateMaze(colIdx, rowIdx, "top", false);
+        updateMaze(colIdx, rowIdx, "bottom", false);
+        updateMaze(colIdx, rowIdx, "visited", false);
+        updateMaze(colIdx, rowIdx, "id", "not-active");
+      });
+    });
   }
 
+  // Set all nodes visited prop to false
+  // Set [15, 15] node id prop to finish
+  // Set visitedCount to 0
   function resetMaze() {
     maze.map((row, rowIdx) => {
       row.map((node, colIdx) => {
         updateMaze(colIdx, rowIdx, "visited", false);
       });
     });
-    updateMaze(14, 14, "id", "finish");
+    updateMaze(15, 15, "id", "finish");
     visitedCount.current = 0;
+    list.current.length = 0;
   }
+
+  // Remove all walls in maze
   function emptyMaze() {
     maze.map((row, rowIdx) => {
       row.map((node, colIdx) => {
@@ -67,40 +93,47 @@ export default function Maze() {
         updateMaze(colIdx, rowIdx, "bottom", true);
         if (rowIdx == 0) {
           updateMaze(colIdx, rowIdx, "top", false);
-        } else if (rowIdx == 14) {
+        } else if (rowIdx == 15) {
           updateMaze(colIdx, rowIdx, "bottom", false);
         }
         if (colIdx == 0) {
           updateMaze(colIdx, rowIdx, "left", false);
-        } else if (colIdx == 14) {
+        } else if (colIdx == 15) {
           updateMaze(colIdx, rowIdx, "right", false);
         }
       });
     });
   }
 
+  // Updates a nodes prop from the given column and row
   function updateMaze(col, row, prop, update) {
     let copyMaze = [...maze];
     copyMaze[col][row][prop] = update;
     setMaze(copyMaze);
   }
 
+  // Return dict of neighbors at given column and row
+  // No neighbor the value will be set to [-1, -1]
   function getNeighbors(col, row) {
     return {
       top: row <= 0 ? [-1, -1] : [col, row - 1],
-      right: col >= 14 ? [-1, -1] : [col + 1, row],
-      bottom: row >= 14 ? [-1, -1] : [col, row + 1],
+      right: col >= 15 ? [-1, -1] : [col + 1, row],
+      bottom: row >= 15 ? [-1, -1] : [col, row + 1],
       left: col <= 0 ? [-1, -1] : [col - 1, row],
     };
   }
 
+  // Returns object with keys of neighbors and boolean values
   function checkNeighbors(col, row, neighbors, solve) {
     return Object.keys(neighbors).filter((direction) => {
       const neighborIdx = neighbors[direction];
 
+      // If no neighbor return false
       if (neighborIdx[0] == -1 || neighborIdx[1] == -1) return false;
 
+      // If function is used for solve and no neighbor
       if (solve && maze[col][row][direction] == false) return false;
+      // If neighbor has not been visited return good neighbor
       else if (!maze[neighborIdx[0]][neighborIdx[1]].visited) {
         const goodNeighbor = maze[neighborIdx[0]][neighborIdx[1]];
         return goodNeighbor;
@@ -109,6 +142,7 @@ export default function Maze() {
     });
   }
 
+  // Sets node id to active and visited to true
   async function setNewNode(col, row) {
     updateMaze(col, row, "id", "active");
     updateMaze(col, row, "visited", true);
@@ -119,40 +153,45 @@ export default function Maze() {
   const list = React.useRef(new Array());
   const visitedCount = React.useRef(0);
 
-  /////////////////////// Recursive Back Track ////////////////////////////////
+  //////////////////////////////////////////////////////////////////
+  /////////////////////// Recursive Back Track /////////////////////
   async function RBT_DFS(col, row) {
-    await setNewNode(col, row);
-    updateMaze(col, row, "id", "not-active");
+    clearMaze();
+    backTrack(col, row);
+    async function backTrack(col, row) {
+      await setNewNode(col, row);
+      updateMaze(col, row, "id", "not-active");
 
-    if (visitedCount.current < maze.length * maze[0].length) {
-      const neighbors = getNeighbors(col, row);
+      if (visitedCount.current < maze.length * maze[0].length) {
+        const neighbors = getNeighbors(col, row);
 
-      const goodNeighbors = checkNeighbors(col, row, neighbors, false);
-      const randDir = getRand(goodNeighbors);
+        const goodNeighbors = checkNeighbors(col, row, neighbors, false);
+        const randDir = getRand(goodNeighbors);
 
-      if (randDir) {
-        list.current.push([col, row]);
-        visitedCount.current = visitedCount.current + 1;
+        if (randDir) {
+          list.current.push([col, row]);
+          visitedCount.current = visitedCount.current + 1;
 
-        const oppD = oppDir[randDir];
-        const randNeighbor = neighbors[randDir];
+          const oppD = oppDir[randDir];
+          const randNeighbor = neighbors[randDir];
 
-        updateMaze(col, row, randDir, true);
-        updateMaze(randNeighbor[0], randNeighbor[1], oppD, true);
+          updateMaze(col, row, randDir, true);
+          updateMaze(randNeighbor[0], randNeighbor[1], oppD, true);
 
-        return RBT_DFS(randNeighbor[0], randNeighbor[1]);
+          return backTrack(randNeighbor[0], randNeighbor[1]);
+        }
+
+        if (list.current.length > 0) {
+          const lastNode = list.current.pop();
+          return backTrack(lastNode[0], lastNode[1]);
+        }
+
+        return resetMaze();
       }
-
-      if (list.current.length > 0) {
-        const lastNode = list.current.pop();
-        return RBT_DFS(lastNode[0], lastNode[1]);
-      }
-
-      return resetMaze();
     }
   }
-  ///////////////////////////////////////////////////////////////////////////////////
-  /////////////////////// Solve Recursive Back Track ////////////////////////////////
+
+  /////////////////////// Solve ////////////////////////////////////
   async function solveRBTDFS(col, row) {
     if (maze[col][row].id == "finish") {
       console.log("FINISH");
@@ -181,45 +220,61 @@ export default function Maze() {
       }
     }
   }
+  /////////////////// Recursive Back Track /////////////////////////
+  //////////////////////////////////////////////////////////////////
 
   //////////////////////////////////////////////////////////////////
   /////////////////////// Prim's ///////////////////////////////////
 
   async function prim(col, row) {
-    await setNewNode(col, row);
-    updateMaze(col, row, "id", "not-active");
+    clearMaze();
+    recurse(col, row);
 
-    const neighbors = getNeighbors(col, row);
-    const goodNeighbors = checkNeighbors(col, row, neighbors, false);
+    async function recurse(col, row) {
+      await setNewNode(col, row);
+      updateMaze(col, row, "id", "not-active");
 
-    goodNeighbors.map((direction) => {
-      neighbors[direction].push(oppDir[direction]);
-      list.current.push(neighbors[direction]);
-    });
+      const neighbors = getNeighbors(col, row);
+      const goodNeighbors = checkNeighbors(col, row, neighbors, false);
 
-    do {
-      if (list.current.length <= 0) return resetMaze();
+      goodNeighbors.map((direction) => {
+        neighbors[direction].push(oppDir[direction]);
+        list.current.push(neighbors[direction]);
+      });
 
-      var randWall = getRand(list.current);
-      var randWallNeighbors = getNeighbors(randWall[0], randWall[1]);
-      var oppWall = randWallNeighbors[randWall[2]];
+      do {
+        if (list.current.length <= 0) {
+          console.log("reset");
+          console.log(list.current);
+          return resetMaze();
+        }
 
-      var index = list.current.indexOf(randWall);
-      if (index > -1) list.current.splice(index, 1);
-    } while (
-      oppWall[0] == -1 ||
-      oppWall[1] == -1 ||
-      maze[randWall[0]][randWall[1]].visited == true
-    );
+        var randWall = getRand(list.current);
+        var randWallNeighbors = getNeighbors(randWall[0], randWall[1]);
+        var oppWall = randWallNeighbors[randWall[2]];
 
-    updateMaze(oppWall[0], oppWall[1], oppDir[randWall[2]], true);
-    updateMaze(randWall[0], randWall[1], randWall[2], true);
+        var index = list.current.indexOf(randWall);
+        if (index > -1) list.current.splice(index, 1);
+      } while (
+        oppWall[0] == -1 ||
+        oppWall[1] == -1 ||
+        maze[randWall[0]][randWall[1]].visited == true
+      );
 
-    return prim(randWall[0], randWall[1]);
+      updateMaze(oppWall[0], oppWall[1], oppDir[randWall[2]], true);
+      updateMaze(randWall[0], randWall[1], randWall[2], true);
+
+      return recurse(randWall[0], randWall[1]);
+    }
   }
+  /////////////////////// Prim's ///////////////////////////////////
   //////////////////////////////////////////////////////////////////
-  //////////////////////////Recursive Division /////////////////////////////
+
+  //////////////////////////////////////////////////////////////////
+  //////////////////////////Recursive Division /////////////////////
   async function recurseDiv() {
+    clearMaze();
+
     function chooseOrientation(width, height) {
       if (width < height) return "horizontal";
       else if (height < width) return "vertical";
@@ -282,13 +337,14 @@ export default function Maze() {
       await divide(nx, ny, w, h, chooseOrientation(w, h));
     }
     emptyMaze();
-    divide(0, 0, 15, 15, chooseOrientation(15, 15));
+    divide(0, 0, 16, 16, chooseOrientation(16, 16));
     return resetMaze();
   }
+  //////////////////////////Recursive Division //////////////////////////
   ///////////////////////////////////////////////////////////////////////
   const styles = {
     display: "inline-grid",
-    gridTemplateColumns: `repeat(${15}, 1fr)`,
+    gridTemplateColumns: `repeat(${16}, 1fr)`,
   };
 
   return (
@@ -296,6 +352,7 @@ export default function Maze() {
       <div className="maze-button-div">
         <div className="maze-generate-div">
           <p className="maze-button-header">Generate</p>
+
           <button className="maze-button" onClick={() => RBT_DFS(0, 0)}>
             DFS
           </button>
